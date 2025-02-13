88571098fe48e2c376ec5a22287462ad2ec3e975
eminyous <youssouf.emine@polymtl.ca>
2025-02-13 18:28:18 -0500

    Use import instead of constant for short module names.

--- src/MomentOpt.jl
+++ src/MomentOpt.jl
@@ -4,28 +4,31 @@ using Reexport
 
 import MathOptInterface as MOI
 
+import MutableArithmetics as MA
 using MutableArithmetics
-const MA = MutableArithmetics
 
 using LinearAlgebra
 
+import MultivariatePolynomials as MP
 using MultivariatePolynomials
-const MP = MultivariatePolynomials
 include("MPext/MPextra.jl")
 
 Reexport.@reexport using JuMP
 
+import SemialgebraicSets as SAS
 Reexport.@reexport using SemialgebraicSets
 include("SASext/SASextra.jl")
 
 using SumOfSquares
 export Sparsity
 
+import MultivariateMoments as MM
 Reexport.@reexport using MultivariateMoments
-const MM = MultivariateMoments
 
+
+import MultivariateBases as MB
 Reexport.@reexport using MultivariateBases
-const MB = MultivariateBases
+
 include("MBext/MBextra.jl")
 
 abstract type AbstractGMPModel <: JuMP.AbstractModel end
