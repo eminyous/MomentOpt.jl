6c87256af143b4ea7904f6fce579ac7e9c5d351b
eminyous <youssouf.emine@polymtl.ca>
2025-02-13 18:44:44 -0500

    Update function signatures to use SAS.AbstractAlgebraicSet

--- src/SASext/SASextra.jl
+++ src/SASext/SASextra.jl
@@ -1,12 +1,12 @@
-function SemialgebraicSets.inequalities(::AbstractAlgebraicSet)
+function SemialgebraicSets.inequalities(::SAS.AbstractAlgebraicSet)
     return []
 end
 
-function polynomials(s::AbstractSemialgebraicSet)
+function polynomials(s::SAS.AbstractSemialgebraicSet)
     return [equalities(s)..., inequalities(s)...]
 end
 
-function MultivariatePolynomials.maxdegree(s::AbstractSemialgebraicSet)
+function MultivariatePolynomials.maxdegree(s::SAS.AbstractSemialgebraicSet)
     pols = polynomials(s)
     return isempty(pols) ? 0 : maximum(maxdegree.(pols))
 end
