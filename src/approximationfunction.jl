4b494efe76f63e69b0847dcefed61189e008edca
eminyous <youssouf.emine@polymtl.ca>
2025-02-13 18:30:53 -0500

    Rename 'basis' field to 'basis_type' in ApproximationFunction struct and update related functions

--- src/approximationfunction.jl
+++ src/approximationfunction.jl
@@ -7,18 +7,18 @@ Type used to store a function defined on elements of basis and returning a Numbe
 struct ApproximationFunction{BT<:MB.AbstractPolynomialBasis}
     func::Function
     vars::Vector{<:MP.AbstractVariable}
-    basis::Type{BT}
+    basis_type::Type{BT}
 end
 
 function (f::ApproximationFunction)(p::AbstractPolynomialLike)
-    basis = maxdegree_basis(f.basis, f.vars, maxdegree(p))
+    basis = maxdegree_basis(f.basis_type, f.vars, maxdegree(p))
     coefs, basis = MB.change_basis(p, basis)
     return dot(coefs, f.func.(basis))
 end
 
 # Linear operations on ApproximationFunctions
 function compatible(f1::ApproximationFunction, f2::ApproximationFunction)
-    return f1.basis == f2.basis && f1.vars == f2.vars
+    return f1.basis_type == f2.basis_type && f1.vars == f2.vars
 end
 
 function Base.sum(v::Vector{<:ApproximationFunction})
@@ -37,7 +37,7 @@ function Base.:+(f1::ApproximationFunction, f2::ApproximationFunction)
     return sum([f1, f2])
 end
 function Base.:*(a::Number, f::ApproximationFunction)
-    return ApproximationFunction(x -> a * f.func(x), f.vars, f.basis)
+    return ApproximationFunction(x -> a * f.func(x), f.vars, f.basis_type)
 end
 Base.:*(f::ApproximationFunction, a::Number) = a * f
 Base.:/(f::ApproximationFunction, a::Number) = inv(a) * f
